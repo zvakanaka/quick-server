8108a83304bb34efb188ccfeef128c98ec1df22a
zvakanaka <quintonish@gmail.com>
2018-09-24 22:40:45 -0600

    basic logging

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const restify = require('restify');
 const server = restify.createServer({ name: process.env.SERVER_NAME || 'quick-server', version: process.env.SERVER_VERSION || '1.0.0' });
 const routes = require('./routes/routes');
 
+server.on('NotFound', function (req, res) {
+  const body = `${req.method} ${req.url} Not Found`;
+  console.log(body);
+  res.writeHead(404, {
+    'Content-Length': Buffer.byteLength(body),
+    'Content-Type': 'text/html'
+  });
+  res.write(body);
+  res.end();
+});
+
 function htmlGet(route, bodyCb) {
   server.get(route, async function (req, res) {
+    console.log(req.method, req.url);
     const body = await bodyCb();
     res.writeHead(200, {
       'Content-Length': Buffer.byteLength(body),
